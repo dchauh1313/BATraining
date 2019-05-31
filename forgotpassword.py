27bbc92ed72259ca6114ee8b9bba5ff63a748f65
dchauh1313 <51221483+dchauh1313@users.noreply.github.com>
2019-05-30 23:28:06 -0400

    Create forgotpassword.py

---
+++ forgotpassword.py
@@ -0,0 +1,6 @@
+print "What if you forget your password"
+X= Hint
+Y= Security Question
+z= Captcha
+Password = Unique code sent to e-mail address
+print "Unique code is a password, but in backwards"
